820cc4a20821ddaf9cad76b10c3ddf8af5865d67
victoria <californie@inbox.ru>
2018-05-28 16:51:36 +0300

    Added making a list of commands

--- main.c
+++ main.c
@@ -5,18 +5,41 @@
 int main()
 {
 	char c, prev;
-	char *buf;
-	int len, n;
+	char *buf, *first, *argv;
+	char **commands;
+	int len, n, nc, argc, i, pid;
+	argc = 0;
 	n = 10;
+	nc = 10;
 	len = 0;
 	prev = ' ';
-	buf = malloc(n * sizeof(char));
+
+	/* parsing */
+	buf = (char*)malloc(n * sizeof(char));
+	commands = (char**)malloc(n * sizeof(char*));
+	for (i = 0; i < nc; i++)
+	{
+		commands[i] = malloc(n*sizeof(char*));
+	}
 	while ((c = getchar()) != EOF)
 		if (((c == ' ') || (c =='\n')) && (prev != ' '))
 		{
 			
-			printf("%s\n", buf);
+			if (argc == nc)
+			{
+				nc = 2 * nc;
+				commands = realloc(commands, nc * sizeof(char*));
+				for (i = 0; i <= nc; i++)
+				{
+					commands[i] = realloc(commands[i], n * sizeof(char*));
+				}
+			}
+			for (i=0; i<len; i++)
+			{
+				commands[argc][i] = buf[i];
+			}
 			free(buf);
+			argc = argc + 1;
 			len = 0;
 			buf = malloc(n * sizeof(char));
 			prev = ' ';
@@ -33,11 +56,21 @@ int main()
 			{
 				n = 2 * n;
 				buf = realloc(buf, n * sizeof(char));
+				for (i = 0; i <= nc; i++)
+				{
+					commands[i] = realloc(commands[i], n * sizeof(char));
+				}
 			}
 			prev = c;
 		}
-		printf("%c", c);
 	printf("I've met EOF");
+	first
+	for (i=0; i<argc; i++)
+	{
+		printf("%s\n", commands[i]);
+	}
+	
+	/* execution */
 	return 0;
 	
 }
